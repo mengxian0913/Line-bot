b21f147a0fc226fa87bc3bfe30e4a8d1fe3cc5aa
mengxain0913 <mengxian0913@gmail.com>
2023-06-14 00:32:42 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
+app = Flask(__name__)
+Channel_Secret = "75e8dc7494a50b1a0c7b5c59abaf799b"
+Channel_AcessToken = "/Ic9cp9b/MMQeuLKaVxTnO7viw7EuFGIuU7jw3NKQob8W2aoMSAUkzkzjXQ79ZyHKls8IxAitL7UhnsexrArSTKJUyEFeC7d4pF3FRPi04fIVfcvArOWvTEdOnUZqQ46LsLKFNHWBNrx83+oWiZrSAdB04t89/1O/w1cDnyilFU="
+line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
+handler = WebhookHandler('YOUR_CHANNEL_SECRET')
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    text = event.message.text
+    reply_text = 'Hello, ' + text
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=reply_text)
+    )
+
+if __name__ == "__main__":
+    app.run()
