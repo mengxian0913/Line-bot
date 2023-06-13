a08b0fb1c1b0118972b80336cda96a5dd01e5e4e
mengxain0913 <mengxian0913@gmail.com>
2023-06-14 02:09:42 +0800

    upload

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
 app = Flask(__name__)
 Channel_Secret = "75e8dc7494a50b1a0c7b5c59abaf799b"
-Channel_AcessToken = "0vEVSYhpMI9ZY4Lp9JwaJ1x9FhnNn2jkTNypCXZlbQcK/8bMes5sWrLIjove5CnWKls8IxAitL7UhnsexrArSTKJUyEFeC7d4pF3FRPi04clSZ2uYsyw2+4ido5TxfWB0ZoHwSSNa3PkJFwrR7ByYwdB04t89/1O/w1cDnyilFU="
-line_bot_api = LineBotApi(Channel_AcessToken)
+Channel_AccessToken = "0vEVSYhpMI9ZY4Lp9JwaJ1x9FhnNn2jkTNypCXZlbQcK/8bMes5sWrLIjove5CnWKls8IxAitL7UhnsexrArSTKJUyEFeC7d4pF3FRPi04clSZ2uYsyw2+4ido5TxfWB0ZoHwSSNa3PkJFwrR7ByYwdB04t89/1O/w1cDnyilFU="
+line_bot_api = LineBotApi(Channel_AccessToken)
 handler = WebhookHandler(Channel_Secret)
 
 @app.route("/", methods=['GET'])
@@ -26,7 +26,6 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     text = event.message.text
-    line_bot_api.send_seen(event.source.user_id)
     reply_text = 'Hello, ' + text
     line_bot_api.reply_message(
         event.reply_token,
