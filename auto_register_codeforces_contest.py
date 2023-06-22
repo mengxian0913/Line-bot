8146e53a60ddb9615f97ebd4c128c24f809c374c
mengxain0913 <mengxian0913@gmail.com>
2023-06-22 12:09:23 +0800

    upload

--- auto_register_codeforces_contest.py
+++ auto_register_codeforces_contest.py
@@ -6,7 +6,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support.wait import WebDriverWait   
 from selenium.webdriver.support import expected_conditions as EC
 
 from linebot.models import (
